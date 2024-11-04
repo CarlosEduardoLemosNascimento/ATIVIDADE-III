ec3e5c54520d0cebcfedf031dac1fe0ebf6a323e
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-11-04 14:16:26 +0000

    .

--- app/models.py
+++ app/models.py
@@ -1,12 +1,23 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Usuario(Base):
     __tablename__ = 'usuarios'
-
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    senha = Column(String(100), nullable=False)  # Armazenar senha de forma segura é recomendado
+    email = Column(String(100), nullable=False)
+    senha = Column(String(100), nullable=False)
+
+def criar_tabela():
+    # Substitua 'mysql+pymysql://user:password@localhost/meu_banco' pelos seus detalhes de conexão
+    engine = create_engine('mysql+pymysql://user:password@localhost/meu_banco')
+    Base.metadata.create_all(engine)
+
+def main():
+    # Chama a função para criar a tabela
+    criar_tabela()
+    
+
+    print("Tabela criada com sucesso!")
