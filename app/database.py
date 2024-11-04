d1f370967bc2f5c15b5e23dbad17d4706c47a648
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-11-04 13:56:00 +0000

    .

--- app/database.py
+++ app/database.py
@@ -2,29 +2,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
-# Parâmetros para conexão com Banco de Dados
+# Parâmetros para conexão com o Banco de Dados
 db_user = "user"
 db_password = "user_password"
-db_host = "db"  # Nome do serviço no docker-compose
+db_host = "localhost"
 db_port = "3306"
 db_name = "meu_banco"
 
-# Endereço/caminho para conexão com banco de dados MySQL. "URL"
+# Endereço/caminho para conexão com o banco de dados MySQL
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-# Conectando ao banco de dados.
+# Conectando ao banco de dados
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
 
-# Gerenciando sessão.
+# Gerenciando a sessão
 @contextmanager
 def get_db():
-    db_session = SessionLocal()
+    db = Session()
     try:
-        yield db_session
-        db_session.commit()  # Se der certo, faz commit.
+        yield db
+        db.commit()  # Se der certo, faz commit.
     except Exception as erro:
-        db_session.rollback()  # Se der erro, desfaz a operação.
-        raise erro  # Lança a exceção de erro.
+        db.rollback()  # Se der erro, desfaz a operação.
+        raise erro  # Lança a exceção de erro
     finally:
-        db_session.close()  # Fechando a conexão.
+        db.close()  # Fechando a conexão
